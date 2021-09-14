377f9fc77d04e10a11a71daadb097d9b62efeba5
Zach Phelan <ZachCPhelan@gmail.com>
2021-09-14 16:18:09 -0600

    Added different test cases

--- alu_testbench.v
+++ alu_testbench.v
@@ -44,12 +44,38 @@ module alutest;
 	);
 
 	initial begin
+		
+		
 //			$monitor("A: %0d, B: %0d, C: %0d, Flags[1:0]:
 //%b, time:%0d", A, B, C, Flags[1:0], $time );
 //Instead of the $display stmt in the loop, you could use just this
 //monitor statement which is executed everytime there is an event on any
 //signal in the argument list.
-
+parameter ADD    = 8'b00000000;
+parameter ADDI   = 8'b00000001;
+parameter ADDU   = 8'b00000010;
+parameter ADDUI  = 8'b00000011;
+parameter ADDC   = 8'b00000100;
+parameter ADDCU  = 8'b00000101;
+parameter ADDCUI = 8'b00000110;
+parameter ADDCI  = 8'b00000111;
+parameter SUB    = 8'b00001000;
+parameter SUBI   = 8'b00001001;
+parameter CMP    = 8'b00001010;
+parameter CMPI   = 8'b00001011;
+parameter CMPU   = 8'b00001100;
+parameter AND    = 8'b00001101;
+parameter OR     = 8'b00001110;
+parameter XOR    = 8'b00001111;
+parameter NOT    = 8'b00010000;
+parameter LSH    = 8'b00010001;
+parameter LSHI   = 8'b00010010;
+parameter RSH    = 8'b00010011;
+parameter RSHI   = 8'b00010100;
+parameter ALSH   = 8'b00010101;
+parameter ARSH   = 8'b00010110;
+parameter NOP    = 8'b00010111;
+		
 		// Initialize Inputs
 		A = 5;
 		B = -7;
@@ -66,15 +92,31 @@ module alutest;
 		//$display("A: %b, B: %b, C:%b, Flags[1:0]: %b, time:%d", A, B, C, Flags[1:0], $time);
 ****/
 		//Random simulation
-		for( i = 0; i< 32; i = i+ 1)
+		$display("Starting tests:");
+		for( i = 0; i< 5000; i = i+ 1)
 		begin
 			#10
-			Opcode = 0;
+			Opcode = ADD;
 			A = i;
 			B = 3*i;
-			
+			#10
 			if (C != A + B)
-				$display("Error: %0d + %0d != %0d, A, B, C");
+				$display("Error: %0d + %0d != %0d", A, B, C);
+			
+			Opcode = SUB // Subtract
+			#10
+			if (C != A - B)
+				$display("Error: %0d - %0d != %0d", A, B, C);
+			
+			Opcode = OR; // Subtract
+			#10
+			if (C != A  | B)
+				$display("Error: %0d | %0d != %0d", A, B, C);
+			
+			Opcode = AND; // Subtract
+			#10
+			if (C != A & B)
+				$display("Error: %0d & %0d != %0d", A, B, C);
 			//B = 2 + i;
 			//$display("A: %0d, B: %0d, C: %0d, Flags[1:0]: %b, time:%0d", A, B, C, Flags[1:0], $time );
 		end
