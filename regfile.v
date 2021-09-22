c799406ec6df58f10ee6a85b350e950fc0a781e5
abporter521 <70152974+abporter521@users.noreply.github.com>
2021-09-22 15:53:44 -0600

    Separate FSM
    Took out the FSM module and put it in its own file.  This will not work in Modelsim but downloads to the board as long as the myFSMBaby file is also located in the project folder.  Separating the module in its own file lets us create multiple different FSM

--- regfile.v
+++ regfile.v
@@ -22,10 +22,11 @@ module RegFile(Clocks, reset, out1, out2, out3, out4);
 	output wire [6:0] out1, out2, out3, out4;
 	wire [3:0] regACont, regBCont;
 	
-	//Finite State Machine
-	myFSMBaby bby(Clocks, reset, RegEnable, regACont, regBCont, opcode); //This is just for a testbench type control
+	//Finite State Machine-FSM is located in another file
+	myFSMBaby1 bby(.clock(Clocks),	.Reset(reset), .regControl(RegEnable), .regACont(regACont), .regBCont(regBCont), .AluOp(opcode)); //Change from 1-2 for different FSM
 	
-	RegBank reg_bank(Bus,r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, RegEnable, Clocks, reset);	FlagReg flags (Clocks, flagswire, flagswire);
+	RegBank reg_bank(Bus,r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, RegEnable, Clocks, reset);	
+	Register flags (flagswire, 1'b1, reset, Clocks, flagswire); //Clocks, flagswire, flagswire);
 	reg_mux regA (r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, regACont,outA);
 	reg_mux regB (r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, regBCont,outB);
 	alu_mux alumux(outB, 4'b0001, opcode, BInput);
@@ -46,79 +47,6 @@ module RegFile(Clocks, reset, out1, out2, out3, out4);
 	
 endmodule
 
-//Stuff needs to be altered.
-module myFSMBaby(clock, Reset, regControl, regACont, regBCont, AluOp);
-
-	input clock, Reset;
-	output reg [15:0] regControl;
-	output reg [7:0] AluOp;
-	output reg [3:0] regACont, regBCont;
-
-	parameter S0=4'd0, S1=4'd1, S2=4'd2, S3=4'd3, S4=4'd4;
-					 
-	reg [3:0] states, S;	//PS - present state, NS
-	
-	initial states = 0;
-	//Determines next state
-	 always @ (posedge clock or negedge Reset) begin
-			  if (!Reset)
-					S <= S0;
-			  else
-					S <= states;
-					
-	end
-
-
-	//Present State becomes next state
-	always@(S)begin
-		case(S)
-						S0: states=S1;
-						S1: states=S2;
-						S2: states=S3;
-						S3: states=S4;
-						S4: states=S4;
-//						S5: states=S6;
-//						S6: states=S7;
-//						S7: states=S8;
-//						S8: states=S9;
-//						S9: states=S10;
-//						S10: states=S11;
-//						S11: states=S12;
-//						S12: states=S13;
-//						S13: states=S14;
-//						S14: states=S14;
-//						default: states = 4'd15;
-					endcase
-	end
-
-	//Output relies only on current state
-	always@(states)begin
-	case (states) //Fibonnacci sequence
-		S0 : begin regControl = 16'h0000; AluOp = 8'd1; regACont = 4'bx; regBCont = 4'bx;end //Intitialize registers to 0
-		S1 : begin regControl = 16'h0003; AluOp = 8'd1; regACont = 4'b0001; regBCont = 4'b0000;end //Inserts 1 into R0 and R1
-		S2 : begin regControl = 16'h0004; AluOp = 8'd0; regACont = 4'b0001; regBCont = 4'b0000;end //Stores R0 + R1 to C and saves in R2
-		S3 : begin regControl = 16'h0008; AluOp = 8'd0; regACont = 4'b0010; regBCont = 4'b0001;end // R1 + R2 = R3		
-		S4 : begin regControl = 16'h0010; AluOp = 8'd0; regACont = 4'b0011; regBCont = 4'b0010;end //R3 + R2 = R4.  AFTER THIS POINT NEED TO MAKE EDITS TO STAGES
-		
-		//Format is regControl enable will store in that register what is in regAcont and regBcont.  
-//		S5 : begin regControl = 16'h0020; AluOp = 8'd0; regACont = 4'b0101; regBCont = 4'b0100;end //Currently will store R5 + R4 into xxxx_xxxx_xx1x_xxxx -> 5th bit or R5
-//		S6 : begin regControl = 16'h0040; AluOp = 8'd0; regACont = 4'b0110; regBCont = 4'b0101;end //Bitwise OR R1+R2
-//		S7 : begin regControl = 16'h0080; AluOp = 8'd0; regACont = 4'b0111; regBCont = 4'b0110;end //Move Output to R1
-//		S8 : begin regControl = 16'h0200; AluOp = 8'd0; regACont = 4'b1000; regBCont = 4'b0111;end //Bitwise Complement
-//		S9 : begin regControl = 16'h0400; AluOp = 8'd0; regACont = 4'b1001; regBCont = 4'b1000;end //Move R3 to R1
-//		S10: begin regControl = 16'h0800; AluOp = 8'd0; regACont = 4'b1010; regBCont = 4'b1001;end //XOR R1 and R2
-//		S11: begin regControl = 16'h1000; AluOp = 8'd0; regACont = 4'b1011; regBCont = 4'b1010;end
-//		S12: begin regControl = 16'h2000; AluOp = 8'd0; regACont = 4'b1100; regBCont = 4'b1011;end
-//		S13: begin regControl = 16'h4000; AluOp = 8'd0; regACont = 4'b1101; regBCont = 4'b1100;end
-//		S14: begin regControl = 16'h8000; AluOp = 8'd0; regACont = 4'b1110; regBCont = 4'b1101;end
-//		//S14: begin regControl = 16'hF; AluOp = 8'd0; regACont = 4'b1111; regBCont = 4'b1110;end
-//				
-//		default : begin  regControl = 3'b010; AluOp = 2'b00; end //Shouldn't happen
-		endcase
-	end
-
-
-endmodule
 
 module FlagReg (Clock, flag_reg_in, flag_reg_out);
 	input Clock;//, enable;
