6be9d7e702df9817b681a8559ca27123483f09b5
abporter521 <70152974+abporter521@users.noreply.github.com>
2021-10-19 16:27:47 -0600

    Create program_counter.v
    Program counter module creation for the first R type instructions where program counter will only increment by 1

---
+++ Lab4/program_counter.v
@@ -0,0 +1,18 @@
+module program_counter(Enable, Reset, program_no); //This program counter is currently 
+
+input wire Enable, Reset;
+output reg [8:0] program_no;  //This is a 9bit number because we will be storing instructions in 512 words in memory. 2**9 = 512.
+
+initial begin
+program_no = 9'd0;
+end
+
+always@(posedge Enable) begin
+
+if(Reset)
+	program_no = program_no + 1;
+else
+	program_no = 9'd0;
+end
+
+endmodule 
