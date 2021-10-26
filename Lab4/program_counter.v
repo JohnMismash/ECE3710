6c0f864059485119dbb209f7313f72b4fd02916b
abporter521 <70152974+abporter521@users.noreply.github.com>
2021-10-25 19:46:46 -0600

    Update program_counter.v

--- Lab4/program_counter.v
+++ Lab4/program_counter.v
@@ -1,10 +1,10 @@
 module program_counter(Enable, Reset, program_no); // This program counter is currently 
 
 input wire Enable, Reset;
-output reg [8:0] program_no;  //This is a 9bit number because we will be storing instructions in 512 words in memory. 2**9 = 512.
+	output reg [9:0] program_no;  //This is a 10 bit number because we will be storing instructions in the first 1024 words in memory. 2**10 = 1024.
 
 initial begin
-program_no = 9'd0;
+program_no = 10'd0;
 end
 
 always@(posedge Enable) begin
@@ -12,7 +12,7 @@ always@(posedge Enable) begin
 if(Reset)
 	program_no = program_no + 1;
 else
-	program_no = 9'd0;
+	program_no = 10'd0;
 end
 
 endmodule
