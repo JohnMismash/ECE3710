15c5b0e39c053281d64580a1e65cabf288fe01e3
ZachPhelan <ZachCPhelan@gmail.com>
2021-10-26 13:31:26 -0600

    Updated tests to have one test for immediate ops and one for all other ops

--- Lab4/assembler_tests.py
+++ Lab4/assembler_tests.py
@@ -40,29 +40,80 @@ class TestAssemblerMethods(unittest.TestCase):
         with self.assertRaises(ValueError):
             assemble(s2)
 
-    def test_assemble_all(self):
+
+    def test_assemble_imm(self):
         op_dict = {
-            'ADD':    '00000000',
             'ADDI':   '00000001',
-            'ADDU':   '00000010',
             'ADDUI':  '00000011',
-            'ADDC':   '00000100',
-            'ADDCU':  '00000101',
             'ADDCUI': '00000110',
             'ADDCI':  '00000111',
-            'SUB':    '00001000',
             'SUBI':   '00001001',
-            'CMP':    '00001010',
             'CMPI':   '00001011',
+            'LSHI':   '00010010',
+            'RSHI':   '00010100',
+        }
+        imm_dict = {
+            '0,':  '0000',
+            '1,':  '0001',
+            '2,':  '0010',
+            '3,':  '0011',
+            '4,':  '0100',
+            '5,':  '0101',
+            '6,':  '0110',
+            '7,':  '0111',
+            '8,':  '1000',
+            '9,':  '1001',
+            '10,': '1010',
+            '11,': '1011',
+            '12,': '1100',
+            '13,': '1101',
+            '14,': '1110',
+            '15,': '1111'
+        }
+        
+        Rdst_dict = {
+            'R0':  '0000',
+            'R1':  '0001',
+            'R2':  '0010',
+            'R3':  '0011',
+            'R4':  '0100',
+            'R5':  '0101',
+            'R6':  '0110',
+            'R7':  '0111',
+            'R8':  '1000',
+            'R9':  '1001',
+            'R10': '1010',
+            'R11': '1011',
+            'R12': '1100',
+            'R13': '1101',
+            'R14': '1110',
+            'R15': '1111'
+        }
+
+        for op in op_dict:
+            for imm in imm_dict:
+                for Rdst in Rdst_dict:
+                    input_string = op + ' ' + imm + ' ' +  Rdst 
+                    correct_answer = op_dict[op] + imm_dict[imm] + Rdst_dict[Rdst]
+
+                    answer = assemble(input_string)
+                    self.assertEqual(correct_answer, answer)
+
+    def test_assemble_non_imm(self):
+        op_dict = {
+            'ADD':    '00000000',
+            'ADDU':   '00000010',
+            'ADDC':   '00000100',
+            'ADDCU':  '00000101',
+            'SUB':    '00001000',
+            'CMP':    '00001010',
             'CMPU':   '00001100',
             'AND':    '00001101',
             'OR':     '00001110',
             'XOR':    '00001111',
             'NOT':    '00010000',
             'LSH':    '00010001',
-            'LSHI':   '00010010',
             'RSH':    '00010011',
-            'RSHI':   '00010100',
             'ALSH':   '00010101',
             'ARSH':   '00010110',
             'NOP':    '00010111'
