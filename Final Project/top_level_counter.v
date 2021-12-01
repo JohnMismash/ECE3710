56ed012761682ba65d524184760cfee00542c32d
JohnMismash <u1179865@utah.edu>
2021-11-30 19:00:09 -0700

    Updated top-level to adhere to the vga clock.

--- Final Project/top_level_counter.v
+++ Final Project/top_level_counter.v
@@ -13,7 +13,7 @@ module top_level_counter(Clk, Reset, Hsync, Vsync, Red, Green, Blue, ClkOut, vga
 	output reg vga_blank;
 	reg [9:0] H_Count, V_Count;
 
-	
+
 	localparam H_DISPLAY   = 640; // Horizontal Display Area
 	localparam H_L_BORDER  =  48; // Horizontal Left Border
 	localparam H_R_BORDER  =  16; // Horizontal Right Border
@@ -33,30 +33,30 @@ module top_level_counter(Clk, Reset, Hsync, Vsync, Red, Green, Blue, ClkOut, vga
                              V_RETRACE - 1;
 	localparam START_V_RETRACE = V_DISPLAY + V_B_BORDER;
 	localparam END_V_RETRACE   = V_DISPLAY + V_B_BORDER + V_RETRACE - 1;
-	
+
 	reg Vert_Counter_Enable;
-	
+
 
 	assign Hsync = (H_Count < H_RETRACE) ? 1'b1:1'b0;
 	assign Vsync = (V_Count < V_RETRACE) ? 1'b1:1'b0;
 
 	always@(posedge Clk) begin
-	
+
 		if (Reset) begin
 			H_Count <= 10'd0;
 			V_Count <= 10'd0;
 		end
-		
+
 		if (H_Count < 799) begin
 			H_Count <= H_Count + 1'b1;
 			Vert_Counter_Enable <= 0;
 		end
-		
+
 		else begin
         H_Count <= 0;
         Vert_Counter_Enable <= 1;
       end
-		
+
 		if (Vert_Counter_Enable == 1'b1) begin
         if (V_Count < 524) begin
             V_Count <= V_Count + 1'b1;
@@ -79,15 +79,15 @@ module top_level_counter(Clk, Reset, Hsync, Vsync, Red, Green, Blue, ClkOut, vga
 		end
 
 		else begin
-		
+
 		// Check for position of each circle
-		
+
 			Red   = 7'h0;
 			Green = 7'h0;
 			Blue = 7'b1;
 		end
 
-		
+
 		ClkOut = ~ClkOut;
 
 	end
