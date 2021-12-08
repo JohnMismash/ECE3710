4063635fc1cfdeaaa64bd1821024f634578b4748
JohnMismash <u1179865@utah.edu>
2021-12-08 10:48:02 -0700

    We can draw a board! Lets goooooo

--- Final Project/bitgen.v
+++ Final Project/bitgen.v
@@ -20,13 +20,17 @@ assign y_pos = vcount;
 
 parameter BLOCK_SIZE_X = 50;
 parameter BLOCK_SIZE_Y = 50;
-parameter GAP_SIZE	= 40;
-parameter INDENT_SIZE_X = 25;
-parameter INDENT_SIZE_Y = 90;
-parameter X_DISTANCE = BLOCK_SIZE_X + GAP_SIZE; // 90
-parameter START_X = 115;
+parameter GAP_SIZE_X	= 30;
+parameter GAP_SIZE_Y = 10;
+parameter INDENT_SIZE_X = 40;
+parameter INDENT_SIZE_Y = 110;
+parameter X_DISTANCE = BLOCK_SIZE_X + GAP_SIZE_X; // 90
+parameter Y_DISTANCE = BLOCK_SIZE_Y + GAP_SIZE_Y; // 90
+parameter START_X = 50;
+parameter START_Y = 90;
 parameter END_X = START_X + BLOCK_SIZE_X; // 165
 integer i;
+integer j;
 
 always @(bright, x_pos, y_pos) begin
 	
@@ -39,49 +43,39 @@ always @(bright, x_pos, y_pos) begin
 	
 	// Display is allowed:
 	if (bright) begin
-		//i = i;
-		if (x_pos >= 0 && x_pos < 640 && y_pos >= 84 && y_pos < 480) begin
-		
-			r = 8'b0;
-			g = 8'b10000000;
-			b = 8'b11111111;
-			//(x_pos >= 25 && x_pos <= 75 && y_pos > 90 && y_pos <= 140)
-			if (x_pos >= INDENT_SIZE_X && x_pos <= BLOCK_SIZE_X + INDENT_SIZE_X && y_pos > INDENT_SIZE_Y && y_pos <= BLOCK_SIZE_Y + INDENT_SIZE_Y) begin
-				r = 8'b01100110;
-				b = 8'b0;
-				g = 8'b0;
-				i = 4'd0;
-			end
+	
+		r = 8'b0;
+		g = 8'b10000000;
+		b = 8'b11111111;
+	
+		for (j = 0; j <=5; j = j + 1) begin
+			if (x_pos >= 0 && x_pos < 640 && y_pos >= 84 && y_pos < 480) begin
 			
-			else begin
-				for (i = 0; i <= 5; i = i + 1) begin
-					if (x_pos > START_X + X_DISTANCE * i && x_pos < END_X + X_DISTANCE * i && y_pos > INDENT_SIZE_Y && y_pos <= BLOCK_SIZE_Y + INDENT_SIZE_Y) begin
+				for (i = 0; i <= 6; i = i + 1) begin
+					if (i == 0) begin
+						if (x_pos >= INDENT_SIZE_X && x_pos <= BLOCK_SIZE_X + INDENT_SIZE_X && y_pos > INDENT_SIZE_Y + Y_DISTANCE * j && y_pos <= BLOCK_SIZE_Y + INDENT_SIZE_Y + Y_DISTANCE * j) begin
+							r = 8'b01100110;
+							b = 8'b0;
+							g = 8'b0;
+						end
+					
+					end
+					
+					if (x_pos > START_X + X_DISTANCE * i && x_pos < END_X + X_DISTANCE * i && y_pos > INDENT_SIZE_Y + Y_DISTANCE * j && y_pos <= BLOCK_SIZE_Y + INDENT_SIZE_Y + Y_DISTANCE * j && i > 0) begin
 						r = 8'b01100110;
 						b = 8'b0;
 						g = 8'b0;
 					end
 				end
 			end
-			
 		
-		// Condition for inner squares
-			// Check memory for if this square is filled and by which color
-				// two if statements, one for each color
-		
-		
-		// Condition for whos turn it is
-		
-		// Condition 
-	
+			// Maroon Background
+			else begin
+				r = 8'b01100110;
+				b = 8'b0;
+				g = 8'b0;
+			end		
 		end
-		
-		// Maroon Background
-		else begin
-			r = 8'b01100110;
-			b = 8'b0;
-			g = 8'b0;
-		end
-		//i = i;
 				
 	end
 	
